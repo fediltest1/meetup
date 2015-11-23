3fba745a4639ba338f7c44f9f977623210d550ea
fediltest1 <fed.il.test1@gmail.com>
2015-11-23 22:48:10 +0000

    chore(bower) add client side dependencies infrastructure. Close #14. [ci skip]

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,11 @@ module.exports = function(grunt) {
     require('load-grunt-initconfig')(grunt);
 
     grunt.registerTask('lint', ['jscs', 'jshint']);
+    grunt.registerTask('dependencies', ['bower']);
     
     grunt.registerTask('default', [
-        'lint'
+        'lint',
+        'dependencies'
     ]);
     
 };
