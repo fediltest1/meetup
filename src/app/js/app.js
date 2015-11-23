4abbc8200fddf83e2c10275d4daa4c27f5e4469f
fediltest1 <fed.il.test1@gmail.com>
2015-11-23 23:37:07 +0000

    feat(app) add app applcation code. Close #16

---
+++ src/app/js/app.js
@@ -0,0 +1,47 @@
+(function(angular) {
+    'use strict';
+
+    function config($routeProvider, $mdThemingProvider) {
+        $routeProvider
+            .when('/login', {
+                templateUrl: 'partials/login.html',
+                controller: 'LoginController',
+                controllerAs: 'vm'
+            })
+            .when('/members', {
+                templateUrl: 'partials/members.html',
+                controller: 'MembersController',
+                controllerAs: 'vm',
+                reloadOnSearch: false
+            })
+            .otherwise('/login');
+
+        $mdThemingProvider.theme('default')
+            .primaryPalette('blue')
+            .accentPalette('red');
+    }
+
+    angular
+        .module('app', [
+            'ngAnimate',
+            'ngAria',
+            'ngMaterial',
+            'ngMdIcons',
+            'ngRoute',
+            'firebase'
+        ])
+        .constant(
+            'baseFirebaseUrl',
+            'https://shining-heat-601.firebaseio.com/'
+        )
+        .constant(
+            'noPhotoUrl',
+            'http://photos4.meetupstatic.com/img/noPhoto_50.png'
+        )
+        .config([
+            '$routeProvider',
+            '$mdThemingProvider',
+            config
+        ]);
+
+}(window.angular));
