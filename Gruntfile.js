49e02c0230862522a6c1c0c2ddbf42e7875397a3
fediltest1 <fed.il.test1@gmail.com>
2015-11-23 23:11:28 +0000

    fix(clean) add clean tast to Gruntfile. Close #15

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,11 @@ module.exports = function(grunt) {
     require('load-grunt-initconfig')(grunt);
 
     grunt.registerTask('lint', ['jscs', 'jshint']);
-    grunt.registerTask('dependencies', ['bower']);
+    grunt.registerTask('dependencies', ['clean:src', 'bower']);
+    grunt.registerTask('test', ['clean:coverage']);
     
     grunt.registerTask('default', [
+        'clean:dist',
         'lint',
         'dependencies'
     ]);
